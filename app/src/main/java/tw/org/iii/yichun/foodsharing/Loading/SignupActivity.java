0a5c12ca7c2bd0ff26e23f06c418207796706a24
xiaolipin <a0979225002@gmail.com>
2020-04-20 23:27:36 +0800

    註冊實現（未完成）

--- app/src/main/java/tw/org/iii/yichun/foodsharing/Loading/SignupActivity.java
+++ app/src/main/java/tw/org/iii/yichun/foodsharing/Loading/SignupActivity.java
@@ -1,9 +1,14 @@
 package tw.org.iii.yichun.foodsharing.Loading;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import tw.org.iii.yichun.foodsharing.R;
 
@@ -11,12 +16,29 @@ import tw.org.iii.yichun.foodsharing.R;
  * 註冊頁面
  */
 public class SignupActivity extends AppCompatActivity {
+
+    TextInputEditText editText;
+    @BindView(R.id.account)
+    EditText account;
+    @BindView(R.id.passwd)
+    EditText passwd;
+    @BindView(R.id.passwd_verify)
+    EditText passwdVerify;
+    @BindView(R.id.phone)
+    EditText phone;
+    @BindView(R.id.captcha)
+    EditText captcha;
+
     @Override
-    protected void onCreate( Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         ButterKnife.bind(this);
+
+
     }
 
 
+    public void Signup_Btn(View view) {
+    }
 }
