96a259506f7098ff103ff4e9b4679acb9ebce090
xiaolipin <a0979225002@gmail.com>
2020-05-15 00:02:54 +0800

    索取紀錄,資料庫已加入

--- app/src/main/java/tw/org/iii/yichun/foodsharing/profile/TakeHistoryFragment.java
+++ app/src/main/java/tw/org/iii/yichun/foodsharing/profile/TakeHistoryFragment.java
@@ -1,10 +1,12 @@
 package tw.org.iii.yichun.foodsharing.profile;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +15,26 @@ import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import tw.org.iii.yichun.foodsharing.Global.MainUtils;
+import tw.org.iii.yichun.foodsharing.Global.Utils;
+import tw.org.iii.yichun.foodsharing.Item.User;
 import tw.org.iii.yichun.foodsharing.R;
 
 /**
@@ -31,8 +49,7 @@ public class TakeHistoryFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_take_history, container, false);
 
         listView = view.findViewById(R.id.takeHistory_lv);
-        List<HashMap<String,Object>> list = getData();
-        listView.setAdapter(new ListViewAdapter(getActivity(), list));
+        sharefoodcard();
 
         return view;
     }
@@ -86,26 +103,124 @@ public class TakeHistoryFragment extends Fragment {
             convertView.setTag(ListItem);
 
             // 綁定資料
-            ListItem.image.setImageResource((Integer)data.get(position).get("image"));
+            ListItem.image.setImageBitmap((Bitmap) data.get(position).get("image"));
             ListItem.title.setText((String)data.get(position).get("title"));
 
             return convertView;
         }
     }
+    /**
+     * 撈已索取完畢的食物
+     */
+    private void sharefoodcard(){
+        String url = "http://"+ Utils.ip +"/FoodSharing_war/getRequestRecord";
+        StringRequest request =  new StringRequest(
+                Request.Method.POST,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
 
+                        JsonFoodcard(response);
+
+                        listView.setAdapter(new ListViewAdapter(getActivity(),list));
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.v("lipin",error.toString());
+                    }
+                }
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> params = new HashMap<>();
+                params.put("userid", User.getId());
+                return params;
+            }
+        };
+
+        MainUtils.queue.add(request);
+
+    }
+
+
+    /**
+     * 抓取json的食物資訊
+     *
+     * @param response
+     */
+    JSONObject row;
+    HashMap<String, Object> hashMap;
+    List<HashMap<String, Object>>list;
+    private void JsonFoodcard(String response) {
+
+        list = new ArrayList<HashMap<String, Object>>();
+
+        try {
+            JSONArray array = new JSONArray(response);
+            for (int i = 0; i < array.length(); i++) {
+                row = array.getJSONObject(i);
+
+                getData(list);//將抓取的值一個一個放在listview裡面
+
+            }
+
+        } catch (Exception e) {
+            Log.v("lipin", "JsonFoodcard:" + e.toString());
+        }
+
+    }
 
     /**
      * 食物 ListView getData
      */
     // TODO: 2020/4/27 撈資料庫資料
-    public List<HashMap<String, Object>> getData(){
-        List<HashMap<String, Object>> list = new ArrayList<HashMap<String, Object>>();
-        for (int i = 0; i < 10; i++){
-            HashMap<String, Object> hashMap = new HashMap<String, Object>();
-            hashMap.put("image", R.drawable.foodimg);
-            hashMap.put("title", "食物名稱" + i);
-            list.add(hashMap);
+    public void getData( List<HashMap<String, Object>>list) throws ParseException {
+        //因為list加入的方式的比對方式是地址,重複的地址物件會被蓋過,所以需要每次尋訪時是產生新的hashMap,故在此new出來
+        hashMap = new HashMap<String, Object>();
+        //拿取拆領的數字,0 or 1
+        int split = Integer.valueOf(row.optString("split"));
+
+        //將拆領的值更改為字串
+        String stringSplit = null;
+        if (split == 1) {
+            stringSplit = "可拆領";
+        } else if (split == 0) {
+            stringSplit = "不可拆領";
         }
-        return list;
+        //將sql回來的截止時間,優化顯示
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        Date date = format.parse(row.optString("due_date"));
+        String dueDate = format.format(date);
+
+        //取出base64的圖片
+        String base64Img = row.optString("foodimg");
+        //轉成bitmap
+        Bitmap bitmap = MainUtils.base64Tobitmap(base64Img);
+
+        //直接將資料顯示在首頁資訊
+
+        hashMap.put("image", bitmap);
+        hashMap.put("title", row.optString("name"));
+
+        //下面東東目的,當客戶端點擊後進入詳細資料時可查看
+        hashMap.put("city", row.optString("city"));
+        hashMap.put("dist", row.optString("dist"));
+        hashMap.put("deadline", dueDate);
+        hashMap.put("quantity", stringSplit);
+        hashMap.put("leftQuantity", row.optString("qty"));
+        hashMap.put("account", row.optString("account"));
+        hashMap.put("username", row.optString("username"));
+        hashMap.put("address", row.optString("address"));
+        hashMap.put("detail", row.optString("detail"));
+        hashMap.put("category", row.optString("category"));
+        hashMap.put("tag", row.optString("tag"));
+        hashMap.put("token", row.optString("token"));
+        hashMap.put("foodid",row.optString("id"));
+        hashMap.put("status",row.optString("status"));
+
+        list.add(hashMap);
     }
 }
